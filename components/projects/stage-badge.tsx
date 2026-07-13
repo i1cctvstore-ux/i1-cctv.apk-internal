cdec4b4ba3c8d1585c0b7e79e6be91a878151e55
i1cctvstore-ux <i1.cctvstore@gmail.com>
2026-07-13 14:51:49 +0700

    Delete components directory

--- components/projects/stage-badge.tsx
+++
@@ -1,6 +0,0 @@
-import { Badge } from '@/components/ui/badge'
-import { stageBadgeClassName, type Stage } from '@/lib/projects'
-
-export function StageBadge({ stage }: { stage: Stage }) {
-  return <Badge className={stageBadgeClassName(stage)}>{stage}</Badge>
-}
